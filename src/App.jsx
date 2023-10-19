a6f3fffda892b9dd4b521393b165d6c4802612df
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-19 23:35:21 +0300

    2.18 aloitettu

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [value, setValue] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    // Fetch country data when the component mounts
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching country data: " + error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Filter countries based on the user's input
+    if (value) {
+      const lowercaseValue = value.toLowerCase();
+      const filtered = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(lowercaseValue)
+      );
+      setFilteredCountries(filtered);
+    } else {
+      setFilteredCountries([]);
+    }
+  }, [value, countries]);
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <div>
+        Country: <input value={value} onChange={handleChange} />
+      </div>
+      {filteredCountries.length < 5 ? (
+        <div>
+          {filteredCountries.length === 0 ? (
+            <p>No matching countries found.</p>
+          ) : (
+            <ul>
+              {filteredCountries.map((country, index) => (
+                <li key={index}>
+                  {country.name.common}
+                  {filteredCountries.length === 1 ? (
+                    <div>
+                      Additional Details:
+                      <ul>
+                        <li>Official Name: {country.name.official}</li>
+                        <li>Capital: {country.capital[0]}</li>
+                        <li>Population: {country.population}</li>
+                        {/* Add more details as needed */}
+                      </ul>
+                    </div>
+                  ) : null}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ) : (
+        <p>Too many matches, please specify another filter.</p>
+      )}
+    </div>
+  );
+}
+export default App;
